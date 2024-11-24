d1919633a6fe51ad4c4100737e2d4fbeb53e0250
Benjamin Collett <benjcollett@gmail.com>
2024-11-24 11:18:53 -0500

    2024-11-24 11:18:53

---
+++ autopush.py
@@ -0,0 +1,69 @@
+#!/bin/python
+import subprocess
+from datetime import datetime
+from tkinter import Tk, Text, Scrollbar, Button
+ignoreNew = {'./.config/nvim/', './.config/fish/', './.config/yazi/'}
+
+
+def get_untracked(stdout: str):
+    out = []
+    print(stdout)
+    for line in stdout.splitlines():
+        if line.startswith('??'):
+            out.append(line[2:])
+    return out
+
+
+def display_popup(message):
+    root = Tk()
+    root.title("Git Status")
+
+    # Create a Text widget for displaying the message
+    text_widget = Text(root, wrap='word', height=15, width=50)
+    text_widget.insert('1.0', message)  # Insert the message into the Text widget
+    text_widget.config(state='disabled')  # Make the text widget read-only
+
+    # Add a Scrollbar to the Text widget
+    scrollbar = Scrollbar(root, command=text_widget.yview)
+    scrollbar.pack(side='right', fill='y')
+    text_widget.config(yscrollcommand=scrollbar.set)
+    scrollbar.pack()
+
+    # Add a button to close the popup
+    close_button = Button(root, text="Close", command=root.quit)
+    close_button.pack()
+
+    root.mainloop()
+
+
+stdout = subprocess.run(['git', 'status', '-s'], capture_output=True, text=True).stdout
+
+
+untracked = get_untracked(stdout)
+
+for path in untracked:
+    temp = './'+path
+    for ignored_paths in ignoreNew[:]:
+        if temp.startswith(ignored_paths):
+            untracked.remove(path)
+
+if stdout == '':
+    exit(0)
+elif len(untracked) >= 1:
+    untracked_string = ''
+    for line in untracked:
+        untracked_string += line+'\r\n'
+    display_popup('Following are untracked:\r\n' + untracked_string)
+else:
+    time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    commit = subprocess.run(['git', 'commit', '-am', time_stamp],
+                          capture_output=True, text=True)
+    commit_error = commit.stderr
+
+    if commit_error != '':
+        display_popup(commit_error)
+    else:
+        push = subprocess.run(['git', 'push'])
+        push_err = push.stderr
+        if push_err != '':
+            display_popup('auto push had an error pushign' + push_err)
