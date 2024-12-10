c3d6773822c832b93e52d8225d6e1722cfc72305
Benjamin Collett <benjcollett@gmail.com>
2024-12-10 08:18:50 -0500

    2024-12-10 08:18:50

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,5 +7,18 @@ config.window_background_opacity = 0.9
 
 config.exit_behavior = "Close"
 
-
+config.keys = {
+    { key = '1', mods = 'CTRL', action = wezterm.action{ActivateTab = 0} },
+    { key = '2', mods = 'CTRL', action = wezterm.action{ActivateTab = 1} },
+    { key = '3', mods = 'CTRL', action = wezterm.action{ActivateTab = 2} },
+    { key = '4', mods = 'CTRL', action = wezterm.action{ActivateTab = 3} },
+    { key = '5', mods = 'CTRL', action = wezterm.action{ActivateTab = 4} },
+    { key = '6', mods = 'CTRL', action = wezterm.action{ActivateTab = 5} },
+    { key = '7', mods = 'CTRL', action = wezterm.action{ActivateTab = 6} },
+    { key = '8', mods = 'CTRL', action = wezterm.action{ActivateTab = 7} },
+    { key = '9', mods = 'CTRL', action = wezterm.action{ActivateTab = 8} },
+    {key = 't', mods = 'CTRL', action = wezterm.action{SpawnTab='CurrentPaneDomain'}},
+    {key="l", mods="ALT", action=wezterm.action{ActivateTabRelative=1}},
+    {key="h", mods="ALT", action=wezterm.action{ActivateTabRelative=-1}},
+   }
 return config
