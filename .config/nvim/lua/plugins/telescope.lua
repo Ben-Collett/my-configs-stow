a90d384acf87886fd2e4fd2219abc3fac17a20e4
Benjamin Collett <benjcollett@gmail.com>
2024-12-28 15:03:21 -0500

    2024-12-28 15:03:21

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -21,10 +21,11 @@ telescope.setup({
     })
     telescope.load_extension('fzf');
 
-    local keymap = vim.keymap 
+    local keymap = vim.keymap
     keymap.set("n", "<leader>fd", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    end, 
+    keymap.set('n','<leader>fh',require('telescope.builtin').help_tags, {desc = "fuz search nvim's help"})
+    end,
     }
