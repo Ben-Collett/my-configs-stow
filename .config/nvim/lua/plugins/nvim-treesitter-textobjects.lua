7f53b79891c3ac8de80a40675a6a213f0f147c43
Benjamin Collett <benjcollett@gmail.com>
2025-03-09 19:22:11 -0400

    2025-03-09 19:22:11

--- .config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
+++ .config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
@@ -1,16 +1,15 @@
 return {
   "nvim-treesitter/nvim-treesitter-textobjects",
-  -- Load when treesitter loads (no need for lazy = true)
-  lazy = true,
   config = function()
-    -- Only modify the textobjects section, don't call full setup
     require("nvim-treesitter.configs").setup({
       textobjects = {
         select = {
           enable = true,
           lookahead = true,
           keymaps = {
-            ["<leader>sf"] = { query = "@function.outer" },
+            ["sf"] = "@function.outer",
+            ["if"] = "@function.inner",
+            -- You can add more text objects here
           },
         },
       },
