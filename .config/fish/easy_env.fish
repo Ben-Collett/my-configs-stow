f32c00bc06362beb4be7fa039cd6701c16c303a1
Benjamin Collett <benjcollett@gmail.com>
2025-07-29 14:51:57 -0400

    2025-07-29 14:51:57

---
+++ .config/fish/easy_env.fish
@@ -0,0 +1,42 @@
+#environment variables
+set -gx APP_IMAGE_PATH ~/Desktop/app_images
+set -gx SOURCE_CODES ~/Desktop/source_codes_for_building
+set -gx EDITOR nvim
+set -gx JAVA_HOME ~/java/bin
+set -gx MANPAGER "nvim +Man!"
+set -gx flutter_path /home/ben/programs/flutter/bin
+
+#aliases
+alias sthing $SOURCE_CODES/syncthing/bin/syncthing
+alias dart /home/ben/programs/flutter/bin/dart
+
+#abbrs
+abbr ls 'eza --icons'
+abbr sl 'eza --icons'
+abbr sls 'eza --icons'
+abbr la 'eza -a --icons'
+abbr al 'eza -a --icons'
+abbr ll 'eza -l --icons'
+abbr lla 'eza -al --icons'
+abbr lal 'eza -al --icons'
+abbr l eza
+abbr lg 'eza|rg'
+abbr lga 'eza -a|rg'
+abbr lt 'eza | tail -n'
+abbr lta 'eza -a| tail -n'
+abbr lh 'eza | head -n'
+abbr lha 'eza -a| head -n'
+abbr sh0 shutdown now
+abbr s0h shutdown now
+abbr 0sh shutdown now
+abbr h0s shutdown now
+abbr shr 'shutdown -r now'
+abbr cl clear
+abbr lc clear
+abbr dc cd
+abbr cn nmtui
+abbr mkp 'mkdir -p'
+abbr cat lolcat
+abbr intellij idea
+abbr fokular org.kde.okular
+abbr clo 'cloc -q --hide-rate .'
