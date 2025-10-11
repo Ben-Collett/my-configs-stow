2ea4b0f8677e4c7606e8c93d2dbbfb7b8fd3c7b5
Benjamin Collett <benjcollett@gmail.com>
2025-10-11 14:25:15 -0400

    2025-10-11 14:25:15

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,15 +9,15 @@ vim.filetype.add({
   },
 })
 
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  callback = function(args)
-    if vim.fn.filereadable(args.file) == 1 then
-      --@diagnostic disable-next-line: missing-fields
-      vim.lsp.start({
-        name = "change_case_lsp",
-        cmd = { "/home/ben/Documents/my_shit/lsp/dart/change_case_lsp/bin/change_case_lsp.exe" },
-        root_dir = vim.fs.dirname(args.file),
-      })
-    end
-  end,
-})
+--vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+-- callback = function(args)
+--  if vim.fn.filereadable(args.file) == 1 then
+--   --@diagnostic disable-next-line: missing-fields
+--  vim.lsp.start({
+--   name = "change_case_lsp",
+--  cmd = { "/home/ben/Documents/my_shit/lsp/dart/change_case_lsp/bin/change_case_lsp.exe" },
+-- root_dir = vim.fs.dirname(args.file),
+--})
+--end
+--end,
+--})
