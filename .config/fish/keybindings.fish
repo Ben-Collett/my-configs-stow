2958b7c0f2e1be3f4d60c0296fed65297d113bcc
Benjamin Collett <benjcollett@gmail.com>
2025-12-20 16:53:11 -0500

    2025-12-20 16:53:11

--- .config/fish/keybindings.fish
+++ .config/fish/keybindings.fish
@@ -3,6 +3,8 @@ bind -e -M insert \cj
 bind -e -M insert \ck
 bind -e -M insert \ch
 bind \cd exit
-bind ctrl-g 'git diff' repaint
+bind -M insert ctrl-g 'echo; git status -s' repaint
+bind -M insert ctrl-f yy repaint
+bind -M insert ctrl-s 'echo; eza --icons' repaint
 bind -M insert \ck up-or-search
 bind -M insert \cj complete
