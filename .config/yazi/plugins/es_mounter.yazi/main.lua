bb17a177e4a03080726adbfbca98cd7f952e07ea
Benjamin Collett <benjcollett@gmail.com>
2026-01-02 12:54:03 -0500

    2026-01-02 12:54:03

--- .config/yazi/plugins/es_mounter.yazi/main.lua
+++
@@ -1,9 +0,0 @@
-return {
-  entry = function ()
-   -- local value, event = ya.input {
-    --  title = "ezmount:",
-    --  position = {'top-center',y=3,w=40},
-    --}
-  ui.List { ui.Text("foo"), ui.Text("bar") }
- end
-}
