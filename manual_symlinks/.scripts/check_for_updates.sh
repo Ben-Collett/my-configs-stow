adbb1cccc8f7314961efae35e779cc9a19454316
Benjamin Collett <benjcollett@gmail.com>
2026-01-14 05:31:51 -0500

    2026-01-14 05:31:51

--- manual_symlinks/.scripts/check_for_updates.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-updates=$(checkupdates 2>/dev/null | wc -l)
-
-if [ "$updates" -gt 0 ]; then
-  hyprctl notify 2 2000000000 "rgba(66ee66ff)" "󰚰  $updates updates available"
-fi
