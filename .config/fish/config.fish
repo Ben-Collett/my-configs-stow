a2b32265ab2f25069800b0ccc25df663f438cc5e
Benjamin Collett <benjcollett@gmail.com>
2026-01-17 12:11:59 -0500

    2026-01-17 12:11:59

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,5 @@ if status is-interactive
 end
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/ben/.ghcup/bin $PATH # ghcup-env
+# opencode
+fish_add_path /home/ben/.opencode/bin
